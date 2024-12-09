78c192d956a332bb9aa033889652ec1d48c2b486
DiegoM <79840160+DiegoMCreator@users.noreply.github.com>
2024-12-09 16:43:30 +0100

    Update python client

--- client/client.py
+++ client/client.py
@@ -1,34 +1,50 @@
 import socket
 import sys
+import threading
 
 def receive_data(s: socket.socket):
-    data = s.recv(4096)
-    print(str(data, "utf-8"))
-
+    while True:
+        try:
+            data = s.recv(4096)
+            if data:
+                print(str(data, "utf-8"))
+            else:
+                print("\nServer disconnected.")
+                s.close()
+                sys.exit()
+        except socket.error as error:
+            print("\nError receiving data: " + str(error))
+            s.close()
+            sys.exit()
 
 def send_command(s: socket.socket):
     while True:
         command = input("-> ")
-        if command == "EXIT":
+        if command.upper() == "EXIT":
             print("Connection closed")
             s.close()
             sys.exit()
         else:
-            s.send(command.encode())
-            receive_data(s)
+            try:
+                s.send(command.encode())
+            except socket.error as error:
+                print("Error sending data: " + str(error))
+                s.close()
+                sys.exit()
 
 def connection(address, client_id):
-    clientName: str = "Client " + str(client_id) 
+    client_name: str = "Client " + str(client_id)
     try:
         s = socket.socket()
         s.connect(address)
         print("Connection to server completed")
-        s.send(clientName.encode())
-        receive_data(s)
+        s.send(client_name.encode())
+        threading.Thread(target=receive_data, args=(s,), daemon=True).start()
 
     except socket.error as error:
-        print("Connection error: " + error)
+        print("Connection error: " + str(error))
         sys.exit()
+
     send_command(s)
 
 if __name__ == "__main__":
